e9957b1bacab61fc76b4c5293596abe06a45d1bb
devahish1099 <42476972+devahish1099@users.noreply.github.com>
2019-01-15 21:14:21 +0530

    completed

--- myScript.js
+++ myScript.js
@@ -2,42 +2,70 @@ var socket = new WebSocket('ws://localhost:8080');
 
 
 socket.onmessage = function(message){
- console.log(message.data);
- var plaintext  = CryptoJS.AES.decrypt(message.data, "chit");
- console.log(plaintext);
- document.getElementById("messages").innerHTML += "<p class = 'received'>" + plaintext.toString(CryptoJS.enc.Utf8) + "</p>" + "<br>";
+      console.log(message);
+      var messageobj = JSON.parse(message.data);
+      console.log(messageobj);
+
+      switch (messageobj.type) {
+        case "msg":
+
+                var plaintext  = CryptoJS.AES.decrypt(messageobj.text, "chit");
+                console.log(plaintext);
+                document.getElementById("messages").innerHTML += "<p class = 'received'>" + plaintext.toString(CryptoJS.enc.Utf8) + "</p>" + "<br>";
+
+          break;
+        case "arr":
+
+                var userList = messageobj.data;
+                console.log(userList);
+                document.getElementById("column").innerHTML = "<p class = 'received'>" + userList + "</p>" + "<br>";
+
+
+          break;
+        default:
+
+      }
 }
 
-//var person = prompt("please enter your name","");
-
+var user = { person:"",type:"", userId:"" , text:""};
 
 
 function getId(){
-var x = document.getElementById("name") ;
-person =  x.value;
-var y = document.getElementById("userId") ;
-var userId = y.value;
-//alert(person);
-var x = document.getElementById("myDialog") ;
-x.close();
+      var loginDiv = document.getElementById("loginpage");
+      loginDiv.style.display = "none";
+      var x = document.getElementById("name") ;
+      user.person =  x.value;
+      var y = document.getElementById("userId") ;
+      user.userId = y.value;
+      alert(user.person);
+      sendId();
+}
+
+function sendId(){
+      user.type = "id";
+      var messageData = JSON.stringify(user);
+      socket.send(messageData);
 }
 
 
 function myfunction() {
-var x = document.getElementById("form1") ;
-var text = "";
-text = x.elements[0].value;
-text = person + " - " + text ;
-var ciphertext = CryptoJS.AES.encrypt(text, "chit");
-socket.send(ciphertext);
-document.getElementById("messages").innerHTML += "<p class = 'sent'>" + text + "</p>" + "<br>";
-document.getElementById("input").value = '';
-};
-
-function loading(){
-	document.getElementById("myDialog").showModal();
+      var x = document.getElementById("form1") ;
+      user.text = x.elements[0].value;
+      user.text = user.person + " - " + user.text ;
+      document.getElementById("messages").innerHTML += "<p class = 'sent'>" + user.text + "</p>" + "<br>";
+      var ciphertext = CryptoJS.AES.encrypt(user.text, "chit").toString();
+      user.text = ciphertext;
+      user.type = "msg";
+      console.log((ciphertext));
+      var messageData = JSON.stringify(user);
+      socket.send(messageData);
+      document.getElementById("input").value = '';
 }
+
+///function loading(){
+	 //   document.getElementById("myDialog").showModal();
+//}
 function onClick(e){
-	//e.preventDefault();
+
 }
 window.addEventListener("keydown", onClick);
--- server1.js
+++ server1.js
@@ -5,21 +5,46 @@ const wss = new WebSocket.Server({port:8080});
 console.log('server is working');
 
 var Connectedusers = 0;
+var users = [];
+
+
 
 wss.on('connection',function connection(ws){
-	ws.on('message', function incoming(message){
-	  console.log('received message:' + message);
-	Connectedusers++;
-	   // Broadcast to everyone else.
-	wss.clients.forEach(function each(client) {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(message);
-         }
-       });
+		Connectedusers++;
+		console.log(Connectedusers);
+		wss.clients.forEach(function each(client) {
+				if (client !== ws && client.readyState === WebSocket.OPEN) {
+					var userList={type:"arr",data:""}
+					userList.data = users;
+					userList = JSON.stringify(userList);
+					client.send(userList);
+					}
+				});
+
+		ws.on('message', function incoming(message){
+				message = JSON.parse(message);
+				console.log(message.type);
+	  		//console.log('received message:' + message);
+				switch (message.type) {
+					case "id":
+							users.push(message.userId);
+					break;
+					case "msg":
+							// Broadcast to everyone else.
+		 					wss.clients.forEach(function each(client) {
+			 						if (client !== ws && client.readyState === WebSocket.OPEN) {
+										message = JSON.stringify(message);
+										client.send(message);
+										}
+									});
+
+					break;
+					default:
+
+		}
+
 	})
 
-	ws.send('you are connected');
-	
+	//ws.send('you are connected');
+
 })
-
-
